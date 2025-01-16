8d78cf1978ffd9ea22f2639a923a6f05756831bf
Alexis Villagran <alexisvillagran18@hotmail.com>
2025-01-16 08:54:51 -0700

    Renamed Module, Added Hader, Added Operator Example

---
+++ OperatorsExample/OperatorsExample.vb
@@ -0,0 +1,15 @@
+﻿'Alexis Villagran 
+'Spring 2025
+'RCET 2265
+'OperatorExample
+'https://github.com/alxsvxn/OperatorsExample.git
+
+Module OperatorsExample
+
+    Sub Main()
+        Console.Write("5 + 3 = ") 'Prints a string of characters in between the quotations
+        Console.WriteLine(5 + 3) 'Print the sum
+
+    End Sub
+
+End Module
