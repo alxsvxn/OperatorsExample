2f9fe92f6101b6ac607c7dca7dfef5a81d5131ca
Alexis Villagran <alexisvillagran18@hotmail.com>
2025-01-17 08:20:19 -0700

    Added more operators

--- OperatorsExample/OperatorsExample.vb
+++ OperatorsExample/OperatorsExample.vb
@@ -7,8 +7,24 @@
 Module OperatorsExample
 
     Sub Main()
+        'Sum Operator
         Console.Write("5 + 3 = ") 'Prints a string of characters in between the quotations
         Console.WriteLine(5 + 3) 'Print the sum
+        Console.WriteLine() 'Adds a blank line under the function
+
+        'Subtraction Operator
+        Console.Write("5 - 3 = ") 'Prints a string of characters in between the quotations
+        Console.WriteLine(5 - 3) 'Print the difference
+        Console.WriteLine()
+
+        'Multiplication Operator
+        Console.Write("5 * 3 = ") 'Prints a string of characters in between the quotations
+        Console.WriteLine(5 * 3) 'Print the product
+        Console.WriteLine()
+
+        'Division Operator
+        Console.Write("5 / 3 = ") 'Prints a string of characters in between the quotations
+        Console.WriteLine(5 / 3) 'Print the quotient
 
     End Sub
 
